6d95eba805dd54be752cbaa9516c09a2ce32c015
Peter Kolloch <kolloch@web-app-evolution.com>
2015-08-30 22:37:31 +0200

    Reexport proto create since usage of rust-mesos depends on it.

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,23 @@
 #[macro_use]
 extern crate hyper;
 extern crate protobuf;
-extern crate proto; // mesos protobuf messages, bad name?
+
+// mesos protobuf messages, bad name?
+//
+// We want to reexport the messages as part of this crate.
+// Otherwise one cannot use the provided interfaces.
+// `pub extern crate` does not work. See https://github.com/rust-lang/rust/issues/21757
+// This is why import crate for a different name here such that we can use pub use below.
+extern crate proto as proto_crate; 
+
 extern crate zookeeper;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
 extern crate log;
 extern crate rustc_serialize;
+
+pub use proto_crate as proto;
 
 mod master_detector;
 pub mod scheduler;
