87a613835f4099964dfd5a776b7539ecc60656b2
bonifaido <bonifaido@gmail.com>
2016-01-27 14:14:39 +0100

    add executor proto

--- proto/generate_rs.sh
+++ proto/generate_rs.sh
@@ -12,9 +12,12 @@ VERSION=0.26.0
 
 cd ../proto/mesos/v1
 curl -O https://raw.githubusercontent.com/apache/mesos/$VERSION/include/mesos/v1/mesos.proto
-cd scheduler
+cd executor
+curl -O https://raw.githubusercontent.com/apache/mesos/$VERSION/include/mesos/v1/executor/executor.proto
+cd ../scheduler
 curl -O https://raw.githubusercontent.com/apache/mesos/$VERSION/include/mesos/v1/scheduler/scheduler.proto
 cd ../../../..
 
 protoc --rust_out src proto/mesos/v1/mesos.proto
+protoc --rust_out src --proto_path=proto proto/mesos/v1/executor/executor.proto
 protoc --rust_out src --proto_path=proto proto/mesos/v1/scheduler/scheduler.proto
